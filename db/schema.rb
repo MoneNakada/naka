83342b003cb8b674f2c921943a60557ec07d2220
MoneNakada <scoups523@gmail.com>
2022-11-23 16:59:34 +0000

    seed追加

--- db/schema.rb
+++ db/schema.rb
@@ -62,11 +62,11 @@ ActiveRecord::Schema.define(version: 2022_11_04_113742) do
   end
 
   create_table "cart_items", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "item_id", null: false
+    t.integer "amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "item_id"
-    t.integer "customer_id"
-    t.integer "amount"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -117,8 +117,6 @@ ActiveRecord::Schema.define(version: 2022_11_04_113742) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.integer "customer_id", null: false
     t.integer "payment_method", default: 0, null: false
     t.string "postal_code", null: false
@@ -127,6 +125,8 @@ ActiveRecord::Schema.define(version: 2022_11_04_113742) do
     t.integer "shipping_cost", null: false
     t.integer "grand_total", null: false
     t.integer "status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,30 @@ Admin.create!(
    email: 'a@a',
    password: 'aaaaaa'
 )
+
+Genre.create!(
+   name: 'ケーキ',
+   is_active: true
+)
+
+item = Item.create!(
+   genre_id: 1,
+   name: 'ショートケーキ',
+   introduction: '1番目の商品です。',
+   price: 500,
+   is_active: true
+)
+item.image.attach(io: File.open(Rails.root.join('app/assets/images/cake1.jpg')), filename: 'cake1.jpg')
+
+Customer.create!(
+   first_name: "難波",
+   last_name: "専太",
+   first_name_kana: "ナンバ",
+   last_name_kana: "センタ",
+   email: "0@0",
+   postal_code: "5420076",
+   address: "大阪府大阪市中央区難波4-4-4 難波御堂筋センタービル 8階",
+   telephone_number: "00088889999",
+   is_active: true,
+   password: "000000",
+)
