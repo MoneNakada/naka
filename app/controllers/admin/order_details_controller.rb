a09a10b71d1b6c2ac7193519d64987b390631345
MoneNakada <scoups523@gmail.com>
2022-11-26 15:00:28 +0000

    ネストなし

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -9,6 +9,15 @@ class Admin::OrderDetailsController < ApplicationController
       @order.update(status: 3)
     end
     redirect_to admin_order_path(@order)
+    
+    # @order_detail = OrderDetail.find(params[:id])
+    # @order_detail.update(order_detail_params)
+    # if @order_detail.making_status == "in_production"
+    #   @order_detail.order.update(status: 2)
+    # elsif OrderDetail.where(making_status: OrderDetail.making_statuses.key(3),order_id: @order_detail.order_id).count == OrderDetail.where(order_id: @order_detail.order_id).count
+    #   @order_detail.order.update(status: 3)
+    # end
+    # redirect_to admin_order_path(@order_detail.order_id)
   end
 
   private
