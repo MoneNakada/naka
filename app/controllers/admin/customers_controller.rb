3273f4e013e1666cee8d1697a20f2fe07feee76a
MoneNakada <scoups523@gmail.com>
2022-11-28 06:47:58 +0000

    会員編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,4 +10,18 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address, :telephone_number, :is_active)
+  end
 end
